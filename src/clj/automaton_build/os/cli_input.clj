fb93ed4fe756ef33c971aa532aa915ed055f9a00
kaspazza <kaspazza@gmail.com>
2024-02-19 11:28:40 +0100

    1.0.1-2-la

--- src/clj/automaton_build/os/cli_input.clj
+++ src/clj/automaton_build/os/cli_input.clj
@@ -4,6 +4,11 @@
 
 (defn user-input "Reads user input" [] (read))
 
+(defn user-input-str
+  "Reads user input, accepts everything and returns always a string"
+  []
+  (read-line))
+
 (defn yes-question
   "Asks user a `msg` and expects yes input. Returns true or false based on the response."
   ([msg force?]
@@ -21,6 +26,6 @@
      (loop []
        (build-terminal-msg/println-msg msg)
        (flush)
-       (let [answer (user-input)]
+       (let [answer (user-input-str)]
          (if (some #(= answer %) options) answer (recur))))))
   ([msg options] (question-loop msg options false)))
