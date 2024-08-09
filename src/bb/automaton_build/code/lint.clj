0c2b69e3af323bf3d21aace04def7c28b7325423
caumond <anthony@caumond.com>
2024-08-09 18:15:52 +0200

    1.0.0

--- src/bb/automaton_build/code/lint.clj
+++
@@ -1,8 +0,0 @@
-(ns automaton-build.code.lint "Lint the application.")
-
-(defn lint-cmd
-  "Lint command in `paths`. If `debug?` is set, that informations are displayed."
-  [debug? paths]
-  (when-not (empty? paths)
-    (-> (concat ["clj-kondo" "--lint"] paths (when debug? ["--debug"]))
-        vec)))
